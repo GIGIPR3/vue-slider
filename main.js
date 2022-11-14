2f7033d3849f8fc7d846eb93c68d58e8a2f1bbc0
GIGIPR3 <gigiprencipe47@gmail.com>
2022-11-14 10:17:10 +0100

    .

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
 var app = new Vue({
-        el: '#root',
-        data: {
-            immagini: [
-                './immagini/immagine1',
+    el: '#root',
+    data: {
+        immagini: [
+            {immagine: './immagini/immagine1.jpg'},
 
-                './immagini/immagine2',
+            {immagine: './immagini/immagine2.jpg'},
 
-                './immagini/immagine3',
+            {immagine: './immagini/immagine3.jpg'},
 
-                './immagini/immagine4',
+            {immagine: './immagini/immagine4.jpg'},
 
-                './immagini/immagine5',
+            {immagine: './immagini/immagine5.jpg'},
 
-            ],
+        ],
 
-            indice: 0,
-        },
-        methods: {
-            prossimaImmagine: function () {
-                let lunghezza = this.immagini.lenght -1;
-                console.log(lunghezza);
+        indice: 0,
+    },
+    methods: {
+        prossimaImmagine: function () {
+            /* let lunghezza = this.immagini.lenght - 1;
+            console.log(lunghezza); */
+
+            if (this.indice < this.immagini.length - 1) {
+                this.indice++;
+            } else {
+                this.indice = 0;
+
             }
         }
     }
-
-);
+})
